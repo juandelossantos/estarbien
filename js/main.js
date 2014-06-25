0af0793d8dd38ab652b6bf8611989544dc7bb0dd
david emilio sierra puentes <sierra.emilio@gmail.com>
2014-06-24 21:32:49 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+function ir(){
+	var target = document.getElementById('formulario');
+	var x = target.offsetLeft;
+	var y = target.offsetTop;
+	console.log(x + "-" + y);
+	var mover = setInterval(function(){
+		for(var i = 0; i<1000; i++){
+			console.log(mover);
+			scrollTo(x,i);
+		}
+	},3000);
+	
+	
+	return false;
+}
 
+window.onload = function() {
+	var source = document.getElementsByClassName('source');
+	console.log(source);
+	source[0].onclick = ir;
+	source[1].onclick = ir;
+	source[2].onclick = ir;
+
+
+}
