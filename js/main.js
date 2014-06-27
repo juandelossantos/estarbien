a8bfa26c9c9ff491e43a6b75106c60726ffa989e
david emilio sierra puentes <sierra.emilio@gmail.com>
2014-06-26 19:55:49 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,37 +1,82 @@
-function ir(id){
 	
-	var target = document.getElementById(id);
-	var y = target.offsetTop;
+// Funciones para escuchar los eventos 
+function agregarEvento(elemento, tipo, funcion){
+	'use strict';
+	if (elemento && elemento.addEventListener) {
+		elemento.addEventListener(tipo, funcion, false)
+	} else if (elemento && elemento.attachEvent) {
+		elemento.attachEvent('on'+tipo,funcion);
+	}
+};
+
+function quitarEvento(elemento, tipo, funcion){
+	'use strict';
+	if (elemento && elemento.removeEventListener) {
+		elemento.removeEventListener(tipo, funcion, false)
+	} else if (elemento && elemento.detachEvent) {
+		elemento.detachEvent('on'+tipo, funcion);
+	}
+}
+
+//Función que hace el scroll vertical de la página de acuerdo al elemento seleccionado
+function ir(target){
+	'use strict';
+	var y = target.offsetTop - 80; // se resta la altura del header para ver adecuadamente los títulos
 	var scrollView = window.pageYOffset;
 	var dif = y-scrollView;
-	console.log(target);
-	console.log("y = "+y+" scrollView = " + scrollView);
-	console.log("y - scrollView = " + dif);
 	
-	
-
+	// aqui se realiza el scroll
 	if (scrollView < y){
-		
 		window.scrollBy(0,12);
-		mover = setTimeout("ir(id)",10);
-		
-
+		var mover = setTimeout(function(){
+			ir(target);	
+		},10);
+		//se asigna la nueva posición de la página a una nueva variable.
+		var newScrollview = window.pageYOffset;
 	} 
-
-	var newScrollview = window.pageYOffset;
-	console.log("nuevo scroll = "+ newScrollview);
-	// if (scrollView === newScrollview) {
-	// 	clearTimeout(mover);
-	// }	
+	//con esta parte se determina si ya se llegó al target deseado y se para la ejecución del timeOut.
+	if (scrollView === newScrollview) {
+		clearTimeout(mover);
+	}	
 	
 }
+/* 	Esta función determina los puntos iniciales y finales para los scroll
+	deseados y ejecuta la funcion ir() que hace el scroll, si se desea incluir más saltos se debe identificar en el html los ids inicio y fin y aquí crear las variables y activar el listener.
+*/
+function defineInicioFinal(){
+	'use strict';
+	var s1 = document.getElementById('source1');
+	var s2 = document.getElementById('source2');
+	var s3 = document.getElementById('source3');
+	
+	var t1 = document.getElementById('target1');
+	var t2 = document.getElementById('target2');
+	var t3 = document.getElementById('target3');
 
-window.onload = function() {
-	var source = document.getElementsByClassName('source');
-	console.log(source);
-	source[0].onclick = ir('formulario');
-	//source[1].onclick = ir;
-	//source[2].onclick = ir;
+	agregarEvento(s1,'click',function(){ir(t1)});
+	agregarEvento(s2,'click',function(){ir(t2)});
+	agregarEvento(s3,'click',function(){ir(t3)});
+	
+} 
 
+function validaForm() {
+	var nombre = document.getElementById('nombre').value;
+	var email = document.getElementById('email').value;
+	var telefono = document.getElementById('telefono').value;
+	var cuando = document.getElementById('cuando').value;
 
+	if (nombre && email && telefono && cuando != " ") {
+		var r =confirm("Enviar?");
+		if (r == true) {alert('enviado');}
+	} else {
+		alert('datos insuficientes');
+	}
+	return false; //cuando se termine de programar la validación quitar el return para que pueda procesar el form en php.
 }
+
+function init(){
+	defineInicioFinal();
+	document.getElementById('invitaForm').onsubmit = validaForm;
+}
+
+window.onload = init;
